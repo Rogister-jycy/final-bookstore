fa4ee40709163641d7281bb767cc9ba2cba0cf46
mengfanHu508 <2601477476@qq.com>
2021-12-30 02:40:54 -0800

    1230

--- MongodbLib.js
+++ MongodbLib.js
@@ -1,45 +1,54 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://172.21.2.236:27017/190110910408');
 
-exports.myinsert = (dbName, mycollection, insertData) => {
-  const MongoClient = require('mongodb').MongoClient;
-  const assert = require('assert');
-
-  const url = 'mongodb://localhost:27017';
-
-  const client = new MongoClient(url, { useUnifiedTopology: true });
-
-  client.connect(function (err) {
-
-    const db = client.db(dbName);
-
-    // Get the documents collection
-    const collection = db.collection(mycollection);
-    // Insert some documents
-    collection.insertMany(insertData, function (err, result) {
-      if (err) console.log('Inserte fail!')
-      else console.log("Inserted 3 documents into the collection");
-      console.log(result);
-      client.close();
-    });
-
-    //client.close();
-  });
+const Userschema = {
+    name: String,
+    pwd: String
 }
 
-exports.myfind = (dbName, mycollection, findData, callback) => {
-  const MongoClient = require('mongodb').MongoClient;
-  const assert = require('assert');
+// exports.myinsert = (dbName, mycollection, insertData) => {
+//   const MongoClient = require('mongodb').MongoClient;
+//   const assert = require('assert');
+  
+//   const url = 'mongodb://172.21.2.236:27017/190110910408';
 
-  const url = 'mongodb://localhost:27017';
-  const client = new MongoClient(url, { useUnifiedTopology: true });
-  client.connect(function (err) {
+//   const client = new MongoClient(url, { useUnifiedTopology: true });
+//   client.connect(function (err) {
 
-    const db = client.db(dbName);
-    const collection = db.collection(mycollection);
-    collection.find(findData).toArray(function (err, docs) {
+//     const db = client.db(dbName);
 
-      console.log(docs);
-      callback(docs);
-    });
+//     // Get the documents collection
+//     const collection = db.collection(mycollection);
+//     // Insert some documents
+//     collection.insertMany(insertData, function (err, result) {
 
-  })
-}
+//       if (err) console.log('Inserte fail!')
+
+//       else console.log("Inserted 3 documents into the collection");
+//       console.log(result);
+//       client.close();
+//     });
+
+//     //client.close();
+//   });
+// }
+
+// exports.myfind = (dbName, mycollection, findData, callback) => {
+//   const MongoClient = require('mongodb').MongoClient;
+//   const assert = require('assert');
+
+//   const url = 'mongodb://172.21.2.236:27017/190110910408';
+//   const client = new MongoClient(url, { useUnifiedTopology: true });
+//   client.connect(function (err) {
+
+//     const db = client.db(dbName);
+//     const collection = db.collection(mycollection);
+    
+//     collection.find(findData).toArray(function (err, docs) {
+
+//       console.log(docs);
+//       callback(docs);
+//     });
+
+//   })
+// }
